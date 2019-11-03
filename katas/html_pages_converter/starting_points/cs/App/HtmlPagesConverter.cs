38745fd912f417c54ae0f4f821c65727eda207e3
Domenic Helfenstein <d.helfenstein@calitime.ch>
2019-11-03 14:30:27 +0100

    Added starting point for HtmlPagesConverter-Kata

---
+++ katas/html_pages_converter/starting_points/cs/App/HtmlPagesConverter.cs
@@ -0,0 +1,46 @@
+﻿namespace App
+{
+    using System.IO;
+
+    public class HtmlPagesConverter
+    {
+        private readonly string filePath;
+
+        public HtmlPagesConverter(
+            string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public string ConvertToHtml()
+        {
+            using (TextReader unicodeFileStream = File.OpenText(this.filePath))
+            {
+                string html = string.Empty;
+
+                string line = unicodeFileStream.ReadLine();
+                while (line != null)
+                {
+                    html += HttpUtility.HtmlEncode(line);
+                    html += "<br />";
+                    line = unicodeFileStream.ReadLine();
+                }
+
+                return html;
+            }
+        }
+
+        private class HttpUtility
+        {
+            public static string HtmlEncode(string line)
+            {
+                line = line.Replace("<", "&lt;");
+                line = line.Replace(">", "&gt;");
+                line = line.Replace("&", "&amp;");
+                line = line.Replace("\"", "&quot;");
+                line = line.Replace("\'", "&quot;");
+                return line;
+            }
+        }
+    }
+}
