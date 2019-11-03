00daa57bf73024b7bd9857c28eacdc718fa85d54
Domenic Helfenstein <d.helfenstein@calitime.ch>
2019-11-03 15:09:29 +0100

    Added a couple of test cases

---
+++ katas/html_pages_converter/solutions/cs/Tests/HtmlPagesConverterFacts.cs
@@ -0,0 +1,59 @@
+﻿namespace Tests
+{
+    using App;
+    using FluentAssertions;
+    using Tests.TestDoubles;
+    using Xunit;
+
+    public class HtmlPagesConverterFacts
+    {
+        [Fact]
+        public void Converter_Creates_LineBreaks()
+        {
+            var lines = new[] { "plain text" };
+            var accessWrapper = new FileAccessWrapperSpy(lines);
+
+            var result = new HtmlPagesConverter(accessWrapper)
+                .ConvertToHtml();
+
+            result.Should().Be("plain text<br />");
+        }
+
+        [Fact]
+        public void Converter_Disposes_AtTheEnd()
+        {
+            var lines = new[] { "plain text" };
+            var accessWrapper = new FileAccessWrapperSpy(lines);
+
+            new HtmlPagesConverter(accessWrapper)
+                .ConvertToHtml();
+
+            accessWrapper.DisposeHasBeenCalled
+                .Should().BeTrue();
+        }
+
+        [Fact]
+        public void Converter_CanConvert_EmptyFile()
+        {
+            var lines = new string[0];
+            var accessWrapper = new FileAccessWrapperSpy(lines);
+
+            var result = new HtmlPagesConverter(accessWrapper)
+                .ConvertToHtml();
+
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Converter_Escapes_Quotes()
+        {
+            var lines = new[] { "text with 'quotes'" };
+            var accessWrapper = new FileAccessWrapperSpy(lines);
+
+            var result = new HtmlPagesConverter(accessWrapper)
+                .ConvertToHtml();
+
+            result.Should().Be("text with &quot;quotes&quot;<br />");
+        }
+    }
+}
---
+++ katas/html_pages_converter/solutions/cs/Tests/TestDoubles/FileAccessWrapperSpy.cs
@@ -0,0 +1,34 @@
+﻿namespace Tests.TestDoubles
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using App;
+
+    public class FileAccessWrapperSpy : IFileAccessWrapper
+    {
+        private readonly IReadOnlyCollection<string> textLines;
+        private int textLineIndex;
+
+        public FileAccessWrapperSpy(
+            IReadOnlyCollection<string> textLines)
+        {
+            this.textLines = new List<string>(textLines);
+            this.textLineIndex = 0;
+        }
+
+        public bool DisposeHasBeenCalled { get; private set; } = false;
+
+        public string GetTextLine()
+        {
+            // Diese Logik macht es schon praktisch zu einem Fake
+            return this.textLines
+                .Skip(this.textLineIndex++)
+                .FirstOrDefault();
+        }
+
+        public void Dispose()
+        {
+            this.DisposeHasBeenCalled = true;
+        }
+    }
+}
