78a488a2db86406c758ee00c48091533f7d7bdda
Domenic Helfenstein <d.helfenstein@calitime.ch>
2019-11-03 14:43:27 +0100

    Created solution with regression protection

---
+++ katas/html_pages_converter/solutions/cs/Tests/RegressionProtection/Roundtrip.cs
@@ -0,0 +1,24 @@
+﻿namespace Tests.RegressionProtection
+{
+    using System.IO;
+    using App;
+    using FluentAssertions;
+    using Xunit;
+
+    public class Roundtrip
+    {
+        [Fact]
+        public void UsingRealFileToTestConvertion()
+        {
+            var fileName = "aFile.txt";
+            var file = File.CreateText(fileName);
+            file.WriteLine("plain text");
+            file.Close();
+
+            var testee = new HtmlPagesConverter(fileName);
+            var convertedText = testee.ConvertToHtml();
+
+            convertedText.Should().Be("plain text<br />");
+        }
+    }
+}
